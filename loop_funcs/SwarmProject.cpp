f5e74566ec65edf86203f386b6665f82966b5ce2
sh3mm <sh3mm@pop-os.localdomain>
2023-04-02 00:08:26 -0400

    addition of a loop function for custom floor

---
+++ loop_funcs/SwarmProject.cpp
@@ -0,0 +1,57 @@
+#include "SwarmProject.h"
+#include <argos3/core/simulator/simulator.h>
+#include <argos3/core/utility/configuration/argos_configuration.h>
+
+/****************************************/
+/****************************************/
+
+SPLoopFunctions::SPLoopFunctions() 
+{}
+
+/****************************************/
+/****************************************/
+
+void SPLoopFunctions::Init(TConfigurationNode& t_node) {
+     m_pcFloor = &GetSpace().GetFloorEntity();
+}
+
+/****************************************/
+/****************************************/
+
+void SPLoopFunctions::Reset() {
+
+}
+
+/****************************************/
+/****************************************/
+
+void SPLoopFunctions::Destroy() {
+   /* Close the file */
+
+}
+
+/****************************************/
+/****************************************/
+
+CColor SPLoopFunctions::GetFloorColor(const CVector2& c_position_on_plane) {
+    if( // center or starting zone
+        (c_position_on_plane.GetY() > -1.0f && c_position_on_plane.GetY() < 1.0f) &&
+        (c_position_on_plane.GetX() > -1.0f && c_position_on_plane.GetX() < 1.0f)
+    ) {
+        return CColor::BLUE;
+    }
+
+    return CColor::WHITE;
+}
+
+/****************************************/
+/****************************************/
+
+void SPLoopFunctions::PreStep() {
+m_pcFloor->SetChanged();
+}
+
+/****************************************/
+/****************************************/
+
+REGISTER_LOOP_FUNCTIONS(SPLoopFunctions, "SP_loop_functions")
